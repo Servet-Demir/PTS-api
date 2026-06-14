34a91401fd534da80c5b0829e5384aa124dd9895
Servet-Demir <demirservet586@gmail.com>
2026-06-14 23:48:40 +0300

    feat: add Birim relation to Personel

--- src/main/java/com/servet/entities/Personel.java
+++ src/main/java/com/servet/entities/Personel.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,4 +29,8 @@ public class Personel {
 
     @Column(nullable = false)
     private Boolean yonetici = false;
+
+    @ManyToOne
+    @JoinColumn(name = "birim_id", nullable = false)
+    private Birim birim;
 }
