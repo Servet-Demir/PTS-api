2bb4228b55fe02866e065f615424efd0bb971e72
Servet-Demir <demirservet586@gmail.com>
2026-06-15 14:48:52 +0300

    refactor: add Lombok annotations to entities

--- src/main/java/com/servet/entities/Birim.java
+++ src/main/java/com/servet/entities/Birim.java
@@ -6,11 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "birimler")
-@Data
 
 public class Birim {
 
--- src/main/java/com/servet/entities/Personel.java
+++ src/main/java/com/servet/entities/Personel.java
@@ -8,11 +8,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Entity
 @Table(name = "personeller")
-@Data
 
 public class Personel {
 
