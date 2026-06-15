ca304587fd61fcd6905154fe6b1401223b122c53
Servet-Demir <demirservet586@gmail.com>
2026-06-15 13:54:25 +0300

    refactor: refactor MaasHesabi.java to use Lombok

--- src/main/java/com/servet/entities/MaasHesabi.java
+++ src/main/java/com/servet/entities/MaasHesabi.java
@@ -12,9 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "maas_hesaplari")
+@Data
+
 public class MaasHesabi {
 
     @Id
