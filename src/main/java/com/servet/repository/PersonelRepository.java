66937017611ffa7f0efd8247e338a6b28d64edfd
Servet-Demir <demirservet586@gmail.com>
2026-06-15 13:58:06 +0300

    feat: add PersonelRepository.java

--- src/main/java/com/servet/repository/PersonelRepository.java
+++ src/main/java/com/servet/repository/PersonelRepository.java
@@ -1,9 +1,10 @@
 package com.servet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.servet.entities.Birim;;
+
+import com.servet.entities.Personel;
 
 // @Repository // yazmaya gerek yok Spring Data JPA bu interfacein bir repo olduğunu anlıtor
-public interface BirimRepository extends JpaRepository<Birim, Long> {
+public interface PersonelRepository extends JpaRepository<Personel, Long> {
 
 }
