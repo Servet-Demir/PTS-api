7cf17f633bb518f0b618563949d5d1189876fb7a
Servet-Demir <demirservet586@gmail.com>
2026-06-15 11:37:27 +0300

    feat: add MaasHesabi.java

---
+++ src/main/java/com/servet/entities/MaasHesabi.java
@@ -0,0 +1,43 @@
+package com.servet.entities;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "maas_hesaplari")
+public class MaasHesabi {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public BigInteger maasId;
+
+    @Column(nullable = false)
+    public BigDecimal brutMaas;
+
+    @Column(nullable = false)
+    public BigDecimal ceza;
+
+    @Column(nullable = false)
+    public LocalDate donem;
+
+    @Column(nullable = false)
+    public Integer gecersizGun;
+
+    @Column(nullable = false)
+    public BigDecimal netMaas;
+
+    @ManyToOne
+    @JoinColumn(name = "personel_id", nullable = false)
+    private Personel personel;
+
+}
