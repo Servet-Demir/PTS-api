3a31cf858fee7d524090f715af1e72714765ca94
Servet-Demir <demirservet586@gmail.com>
2026-06-17 09:19:41 +0300

    docs: add inline comments to BirimRepository.java

--- src/main/java/com/servet/repository/BirimRepository.java
+++ src/main/java/com/servet/repository/BirimRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.servet.entities.Birim;;
 
 // @Repository // yazmaya gerek yok Spring Data JPA bu interfacein bir repo olduğunu anlıtor
-public interface BirimRepository extends JpaRepository<Birim, Long> {
+public interface BirimRepository extends JpaRepository<Birim, Long> { // virgülden önceki kısıma hangi sınıf üzerinde
+                                                                      // işlem yapacağımızı yazıyoruz. ikinci kısımda
+                                                                      // ise o sınıfın primary keyinin tipini yazıyoruz.
+                                                                      // JpaRepository bize hazır olarak CRUD
+                                                                      // işlemlerini yapabileceğimiz metotları sağlar.
+                                                                      // İsterseniz kendi özel sorgularınızı da
+                                                                      // ekleyebilirsiniz.
 
 }
