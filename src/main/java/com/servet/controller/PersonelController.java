1e1eceb9b3bf8f50bf449e9a2f1f5dc82e842cfd
Servet-Demir <demirservet586@gmail.com>
2026-06-18 13:40:28 +0300

    feat: add contents to PersonelController.java

---
+++ src/main/java/com/servet/controller/PersonelController.java
@@ -0,0 +1,49 @@
+package com.servet.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.servet.entities.Personel;
+import com.servet.services.PersonelService;
+
+@RestController
+@RequestMapping("/rest/api/personel")
+public class PersonelController {
+
+    @Autowired
+    private PersonelService personelService;
+
+    @GetMapping("/list")
+    public List<Personel> getAllPersonelList() {
+        return personelService.getAllPersonelList();
+    }
+
+    @GetMapping("/list/{id}")
+    public Personel getPersonelById(@PathVariable Long id) {
+        return personelService.getPersonelById(id);
+    }
+
+    @PostMapping("/save")
+    public Personel savePersonel(@RequestBody Personel personel) {
+        return personelService.savePersonel(personel);
+    }
+
+    @PutMapping("/update/{id}")
+    public Personel updatePersonel(@PathVariable Long id, @RequestBody Personel personel) {
+        return personelService.updatePersonel(id, personel);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean deletePersonel(@PathVariable Long id) {
+        return personelService.deletePersonel(id);
+    }
+}
