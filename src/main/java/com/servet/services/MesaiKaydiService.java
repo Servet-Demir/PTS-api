3ecb6540f36c1f0c2f5a93eba0829004b1e627a4
Servet-Demir <demirservet586@gmail.com>
2026-06-19 13:18:26 +0300

    chore: delete Autowired import

--- src/main/java/com/servet/services/MesaiKaydiService.java
+++ src/main/java/com/servet/services/MesaiKaydiService.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.servet.entities.MesaiKaydi;
--- src/main/java/com/servet/services/PersonelService.java
+++ src/main/java/com/servet/services/PersonelService.java
@@ -2,7 +2,6 @@ package com.servet.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.servet.entities.Personel;
